16d379a2a52a2bc639393c5e4c36917009ebcf13
unknown <domogatskiy.a@yandex.ru>
2022-05-09 13:39:09 +0300

    feat: add project to gh-pages #0

--- src/assets/scripts/app.js
+++
@@ -1,7 +0,0 @@
-import '../styles/style.css'
-import { Keyboard } from './Keyboard'
-import engKeys from '../eng.json'
-import rusKeys from '../rus.json'
-
-const iKeyboard = new Keyboard({ eng: engKeys, rus: rusKeys })
-iKeyboard.renderInterfaceKeyboard()
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './assets/styles/style.css'
-
-const y = 2
-console.log(y)
-if (y) {
-    y = 3
-}
